56aa79257cfce471f16173eb133178d33aca0e88
zhangxinnan123 <zhan9359@umn.edu>
2026-04-13 17:47:17 -0500

    update script

--- jx_experiment/config.sh
+++ jx_experiment/config.sh
@@ -3,6 +3,7 @@
 # Edit this file to set your credentials and project settings.
 
 WANDB_API_KEY="c4b67c713ad88ef65b62908bcaa8b5c5cb72d1a9"
+export WANDB_ENTITY="${WANDB_ENTITY:-rl_agent}"
 PROJECT_NAME="verl_opd_dapo"
 
 # Activate conda environment
