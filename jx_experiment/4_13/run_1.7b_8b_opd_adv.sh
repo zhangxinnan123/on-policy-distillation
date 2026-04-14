7bdcaa879167333a5b1e64129e00133f2d57facc
zhangxinnan123 <zhan9359@umn.edu>
2026-04-13 23:08:27 -0500

    update script

--- jx_experiment/4_13/run_1.7b_8b_opd_adv.sh
+++ jx_experiment/4_13/run_1.7b_8b_opd_adv.sh
@@ -3,7 +3,7 @@ set -xeuo pipefail
 
 ############################ Quick Config ############################
 
-source "$(dirname "$0")/config.sh"
+source "$(dirname "$0")/../config.sh"
 
 ROLLOUT_NAME="vllm" # sglang or vllm
 
@@ -32,18 +32,22 @@ STUDENT_MICRO_BATCH_SIZE_PER_GPU=4
 STUDENT_MAX_TOKEN_LEN_PER_GPU=$(( STUDENT_MICRO_BATCH_SIZE_PER_GPU * (MAX_PROMPT + MAX_RESPONSE_LENGTH) ))
 USE_DYNAMIC_BSZ=True
 
-STUDENT_WORLD_SIZE=4
+STUDENT_WORLD_SIZE=2
 
-TEACHER_RESOURCE_POOL=False
-TEACHER_WORLD_SIZE=4
+TEACHER_RESOURCE_POOL=True
+TEACHER_WORLD_SIZE=2
 
 SP=1
 
-## advantage clip
-advantage_mask_low=-0.5
-advantage_mask_high=0.05
 
-EXP_NAME="fsdp/student-${STUDENT_MODEL}/teacher-${TEACHER_MODEL}/loss-${DISTILLATION_LOSS_MODE}/pg-${USE_POLICY_GRADIENT}/adv_mask-${advantage_mask_low}_${advantage_mask_high}"
+ROLLOUT_N=4
+LR=1e-6
+train_batch_size=$(( TRAIN_PROMPT_BSZ * ROLLOUT_N ))
+## advantage clip
+advantage_mask_low=-1
+advantage_mask_high=0.1
+
+EXP_NAME="fsdp/student-${STUDENT_MODEL}/teacher-${TEACHER_MODEL}/loss-${DISTILLATION_LOSS_MODE}/pg-${USE_POLICY_GRADIENT}_adv_mask-${advantage_mask_low}_${advantage_mask_high}_b${train_batch_size}_n${ROLLOUT_N}_lr${LR}"
 
 ENFORCE_EAGER=True # true for faster debugging
 
@@ -94,7 +98,7 @@ DISTILLATION=(
     distillation.teacher_model.model_path="${TEACHER_MODEL}"
     distillation.teacher_model.inference.tensor_model_parallel_size=1
     distillation.teacher_model.inference.name=$ROLLOUT_NAME
-    distillation.teacher_model.inference.gpu_memory_utilization=0.5
+    distillation.teacher_model.inference.gpu_memory_utilization=0.8
     distillation.teacher_model.inference.enforce_eager=$ENFORCE_EAGER
     distillation.teacher_model.inference.max_model_len=$MAX_NUM_TOKENS
     distillation.teacher_model.inference.max_num_batched_tokens=$MAX_NUM_TOKENS
@@ -108,7 +112,7 @@ DISTILLATION=(
 )
 
 STUDENT=(
-    actor_rollout_ref.actor.optim.lr=1e-6
+    actor_rollout_ref.actor.optim.lr=$LR
     actor_rollout_ref.actor.ppo_mini_batch_size=$TRAIN_PROMPT_BSZ
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=$STUDENT_MICRO_BATCH_SIZE_PER_GPU
     actor_rollout_ref.actor.ppo_max_token_len_per_gpu=$STUDENT_MAX_TOKEN_LEN_PER_GPU
@@ -126,12 +130,12 @@ ROLLOUT=(
     actor_rollout_ref.rollout.log_prob_use_dynamic_bsz=$USE_DYNAMIC_BSZ
     actor_rollout_ref.rollout.tensor_model_parallel_size=1
     actor_rollout_ref.rollout.name=$ROLLOUT_NAME
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.4
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.8
     actor_rollout_ref.rollout.calculate_log_probs=False
     actor_rollout_ref.rollout.max_model_len=$MAX_NUM_TOKENS
     actor_rollout_ref.rollout.max_num_batched_tokens=$MAX_NUM_TOKENS
     actor_rollout_ref.rollout.max_num_seqs=$MAX_NUM_SEQS
-    actor_rollout_ref.rollout.n=4
+    actor_rollout_ref.rollout.n=${ROLLOUT_N}
     actor_rollout_ref.rollout.val_kwargs.temperature=${temperature} \
     actor_rollout_ref.rollout.val_kwargs.top_p=${val_top_p} \
     actor_rollout_ref.rollout.val_kwargs.top_k=${top_k} \
@@ -155,8 +159,8 @@ TRAINER=(
     trainer.nnodes=1
     trainer.save_freq=-1
     trainer.test_freq=20
-    trainer.total_epochs=3
-    trainer.val_before_train=False
+    trainer.total_epochs=1
+    trainer.val_before_train=True
     trainer.use_legacy_worker_impl=disable
     trainer.resume_mode=disable
     trainer.log_val_generations=5
